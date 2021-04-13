36939b5748cd7a7e3e97ff197976b939043ee074
Christina Giannouli <xristitina@gmail.com>
2021-04-13 22:08:31 +0300

    Add style related loaders to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,23 @@ module.exports = {
                                   title: 'Movierama in Vanilla JS',
                                   meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
                               })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Successfully use url() in variables and mixins
+                    "resolve-url-loader",
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            }
+        ],
+    },
 };
 
