e48616d42b5cf049b9f14c13be0a89d8bc7a7d54
Christina Giannouli <xristitina@gmail.com>
2021-04-13 23:14:59 +0300

    Configure babel in webpack rules

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,18 @@ module.exports = {
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 type: 'asset',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
             }
         ]
     }
