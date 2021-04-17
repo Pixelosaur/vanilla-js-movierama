a32813831cf19f740844576b18ad2d6bc75e2fb8
Christina Giannouli <xristitina@gmail.com>
2021-04-17 22:25:11 +0300

    Initialize MoviesModule after main App component has rendered

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,9 +1,11 @@
 import AppTemplate from './app.template';
+import MoviesModule from './movies/movies.module';
 
 const AppComponent = {
   init() {
     this.appElement = document.querySelector('#app');
     this.render();
+    MoviesModule.init();
   },
 
   render() {
