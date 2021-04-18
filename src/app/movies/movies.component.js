984c081e341b4d71298bc8eed3de3edd54677ec3
Christina Giannouli <xristitina@gmail.com>
2021-04-18 22:15:29 +0300

    Implement infinite scroll functionality

--- src/app/movies/movies.component.js
+++ src/app/movies/movies.component.js
@@ -7,18 +7,32 @@ import './movie/movie.template.scss';
 import '../shared/loader/loader.template.scss';
 
 let showLoader = true;
+let pageNumber = 1;
+const movieList = [];
 
 const MoviesComponent = {
   init() {
-    this.appElement = document.querySelector('.container');
     this.render();
+    this.loadMore();
+  },
+
+  loadMore() {
+    window.addEventListener('scroll', () => {
+      const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+      if (scrollTop + clientHeight > scrollHeight - 5) {
+        pageNumber += 1;
+        setTimeout(this.render, 500);
+      }
+    });
   },
 
   async render() {
+    this.appElement = document.querySelector('.container');
+
     if (showLoader) {
       this.appElement.innerHTML = Loader();
     }
-    const movieList = await MoviesService.getMovieList();
+    movieList.push(...(await MoviesService.getMovieList(pageNumber)));
     const movies = movieList.reduce((html, movie) => `${html} ${MovieTemplate(movie)}`, '');
 
     showLoader = false;
