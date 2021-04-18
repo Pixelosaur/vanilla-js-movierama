f6b55970e5ec1f7e913a122d94f27a711ecea1a1
Christina Giannouli <xristitina@gmail.com>
2021-04-18 19:24:36 +0300

    Update movie template to display all needed info

--- src/app/movies/movie/movie.template.js
+++ src/app/movies/movie/movie.template.js
@@ -1,7 +1,31 @@
 const MovieTemplate = (movie) => `
     <div class="movie-item">
-       <h4>${movie.title}</h4>
-       <img src=${movie.fullPosterPath} alt=${movie.title}>
+        <div class="overview">
+            <div class="meta">
+                <span>
+                    <i class="far fa-calendar-alt"></i>
+                    <span>${movie.yearOfRelease}</span>
+                </span>
+                
+                <span>
+                    <i class="far fa-star"></i>
+                    <span>${movie.vote_average}</span>
+                </span>
+            </div>
+            <div class="poster">
+                <img src=${movie.fullPosterPath} alt=${movie.title}>
+            </div>
+        </div>
+        <div class="genres">
+            ${movie.genres.reduce(
+              (html, genre) => `${html} <span class="badge">${genre}</span>`,
+              '',
+            )}
+        </div>
+        <div class="description">
+            <h4 class="title">${movie.title}</h4>
+            <p>${movie.overview}</p>
+        </div>
     </div>
 `;
 
