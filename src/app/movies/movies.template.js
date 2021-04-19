60846fa8f01a1f474ffee2ebe86eece9c19b2351
Christina Giannouli <xristitina@gmail.com>
2021-04-19 19:27:36 +0300

    Add search input to movies template

--- src/app/movies/movies.template.js
+++ src/app/movies/movies.template.js
@@ -1,5 +1,8 @@
 const MoviesTemplate = (movies) => `
     <div class="movies">
+        <div class="movie-search">
+            <input id="search" type="search" name="search" value="" placeholder="Search..." />
+        </div>
         ${movies}
     </div>
 `;
